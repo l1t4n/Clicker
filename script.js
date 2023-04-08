9c5ec077bffd6e4a1ba593362265a3a00694b86a
l1t4n <maxivan200206@gmail.com>
2023-04-08 17:08:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// тут как понятно я пытался переменные сделать нашего персонажа и врага,
+// лучше чет в голову ничего не пришло, может через классы будет лучше )
+let hero = {
+    hp: 100,
+    attack: 10
+}
+
+let enemy = {
+    hp: 100,
+    attack: 10
+}
+
+
+
+// тут должен быть звук выстрела при клике ↙
+document.addEventListener('click', () => {
+    const audio = new Audio();
+    audio.src = '**hit.mp3**';
+    audio.volume = 0.05;
+    audio.play
+})
+
